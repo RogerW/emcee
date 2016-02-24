2b7fad7d3a31c88d24137b5268024f1cb1cb4f33
Roger_Wilco <stsvorobev@gmail.com>
2016-02-24 09:25:26 +0000

    fix support sprockets 3

--- lib/emcee/helpers/sprockets_helper.rb
+++ lib/emcee/helpers/sprockets_helper.rb
@@ -8,20 +8,27 @@ module Sprockets
       # Based on Sprocket's javascript_include_tag.
       def html_import_tag(*sources)
         options = sources.extract_options!.stringify_keys
+        integrity = compute_integrity?(options)
+
         if options["debug"] != false && request_debug_assets?
           sources.map { |source|
-            #check_errors_for(source)
-            if asset = lookup_debug_asset(source, type: :html)
-              asset.to_a.map do |a|
-                super(path_to_html(a.logical_path, debug: true), options)
+            if asset = lookup_debug_asset(source, :type => :html)
+              if asset.respond_to?(:to_a)
+                asset.to_a.map do |a|
+                  super(path_to_html(a.logical_path, :debug => true), options)
+                end
+              else
+                super(path_to_html(asset.logical_path, :debug => true), options)
               end
             else
               super(source, options)
             end
           }.flatten.uniq.join("\n").html_safe
         else
-          sources.push(options)
-          super(*sources)
+          sources.map { |source|
+            options = options.merge('integrity' => asset_integrity(source, :type => :html)) if integrity
+            super source, options
+          }.join("\n").html_safe
         end
       end
     end
